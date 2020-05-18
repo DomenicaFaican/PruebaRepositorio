a377b58ea974d3dd6dab201b3946f3234f38dc36
DomenicaFaican <domefai@hotmail.com>
2020-05-18 09:24:28 -0500

    Primer Commit TrabajoGitHub

--- TrabajoGitHubApp/src/trabajogithubapp/GestorTrabajoGitHubApp.java
+++ TrabajoGitHubApp/src/trabajogithubapp/GestorTrabajoGitHubApp.java
@@ -16,6 +16,9 @@ public class GestorTrabajoGitHubApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola a todos");
+        
+              
     }
     
 }
